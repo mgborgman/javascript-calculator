db721e2acf6c7e910fb479b202d7dbcb51ee09b8
mgborgman <mgborgman#gmail.com>
2022-11-17 05:49:54 -0800

    Need to work out a few bugs.

--- script.js
+++ script.js
@@ -35,13 +35,35 @@ function operate(operator, num1, num2) {
     } 
 }
 
+function clearScreen() {
+    calculatorDisplay.innerHTML = '';
+}
+
+function clearScreenAndCurrentVariable() {
+    if(typeof operand1 !== 'undefined' && operator == '') {
+        operand1 = [];
+    } else if(typeof operand1 !== 'undefined' && operator !== '') {
+        operand2 = [];
+    }
+    calculatorDisplay.innerHTML = '';
+}
+
+function allClear() {
+    operand1 = ['0'];
+    operand2 = ['0'];
+    operator = '';
+    result = 0;
+}
+
 let buttons = document.querySelectorAll('#calculator-container button');
 // let calculatorOperatorButtons = document.querySelectorAll('#calculator-container .operator');
 let calculatorDisplay = document.getElementById('calculator-display');
-let operand1 = [];
-let operand2 = [];
+let decimalButton = document.getElementById('decimal');
+let operand1 = ['0'];
+let operand2 = ['0'];
 let operator = '';
 let result;
+calculatorDisplay.innerHTML = operand1;
 
 for(const button of buttons) {
     button.addEventListener("click", function() {
@@ -57,23 +79,40 @@ for(const button of buttons) {
             case '7':
             case '8':
             case '9':
-                if(operand1.length == 0) {
+            case '.':
+                if(operand1.length == 1 && (operand1[0] == '0' || operand1[0] == 0) && operator == '') {
+                    const index = operand1.indexOf('0');
+                    operand1.splice(index, 1, button.value);
+                    calculatorDisplay.innerHTML = button.value;
+                } else if(operand1.length >= 1 && operator == '') {
                     operand1.push(button.value);
+                    if(operand1.includes('.')) {
+                        decimalButton.disabled = true;
+                    } else {
+                        decimalButton.disabled = false;
+                    }
                     calculatorDisplay.innerHTML += button.value;
                 } else {
+                    decimalButton.disabled = false;
                     operand2.push(button.value);
-                    calculatorDisplay.innerHTML = button.value;
+                    if(operand2.includes('.')) {
+                        decimalButton.disabled = true;
+                    } else {
+                        decimalButton.disabled = false;
+                    }
+                    clearScreen();
+                    calculatorDisplay.innerHTML += button.value;
                 }
                 break;
             case '+':
             case '-':
             case '/':
             case '*':
-                if(operand2.length == 0) {
+                if(operand2.length == 1 && operand2[0] == '0') {
                     operand1 = operand1.join('');
                 } else {
                     operand1 = operate(operator, operand1, operand2);
-                    operand2 = [];
+                    operand2 = ['0'];
                 }
                 operator = button.value;
                 break;
@@ -83,9 +122,16 @@ for(const button of buttons) {
                 result = operate(operator, +operand1, +operand2);
                 calculatorDisplay.innerHTML = result;
                 operand1 = [result]
-                operand2 = [];
+                operand2 = ['0'];
                 operator = '';
                 break;
+            case 'c':
+                clearScreenAndCurrentVariable();
+                break;
+            case 'ac':
+                clearScreen();
+                allClear();
+                calculatorDisplay.innerHTML = operand1;
             default:
 
         }
